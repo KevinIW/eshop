b78a7aad0a543b8719735f66b571dd64fa4a3a4b
Kevin I <kevin.ignatius@ui.ac.id>
2024-03-01 17:11:05 +0700

    [REFACTOR] Apply OrderStatus enum check to Order model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -3,6 +3,8 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import java.util.Arrays;
 import java.util.List;
+
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import lombok.*;
 
 @Builder
@@ -12,44 +14,31 @@ public class Order {
     List<Product> products;
     long orderTime;
     String author;
-
     String status;
 
     public Order(String id, List<Product> products, long orderTime, String author) {
-
         this.id = id;
         this.orderTime = orderTime;
         this.author = author;
-        this.status = "WAITING_PAYMENT";
+        this.status = OrderStatus.WAITING_PAYMENT.getValue();
 
         if (products.isEmpty()) {
             throw new IllegalArgumentException();
         } else {
             this.products = products;
         }
-
     }
 
     public Order(String id, List<Product> products, long timestamp, String author, String status) {
         this(id, products, timestamp, author);
-        String[] statusList = {"WAITING_PAYMENT","FAILED", "SUCCESS", "CANCELLED"};
-
-        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
-            throw new IllegalArgumentException();
-        } else {
-            this.status = status;
-        }
-
+        this.setStatus(status);
     }
 
     public void setStatus(String status) {
-        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
-        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
-            throw new IllegalArgumentException();
-        } else {
+        if (OrderStatus.contains(status)) {
             this.status = status;
+        } else {
+            throw new IllegalArgumentException();
         }
     }
-
-
 }
